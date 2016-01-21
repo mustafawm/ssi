a619418268b44d1b63c50f14b090322309163842
Mustafa Al-Saegh <mustafawm@msn.com>
2016-01-22 00:52:48 +1100

    fixed: moved security check to users route

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,6 @@ var express  = require('express');
 var router   = express.Router();
 var passport = require('passport');
 
-// security middleware
-router.use('/', function(req, res, next) {
-	if ( ! req.user ) {
-		res.redirect('/');
-	}
-	next();
-});
-
 
 router.route('/google/callback')
 	.get(passport.authenticate('google', {
